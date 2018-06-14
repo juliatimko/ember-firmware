36eeeb2d6cd9bcdb47ce82314f7fde727b88e334
Julia Timko <JuliaTimko@dhcp-rhodes-1399.eduroam.cornell.edu>
2018-06-14 16:07:54 -0400

    alter min reading

--- C++/Screen.cpp
+++ C++/Screen.cpp
@@ -4,7 +4,7 @@
 //  This file is part of the Ember firmware.
 //
 //  Copyright 2015 Autodesk, Inc. <http://ember.autodesk.com/>
-//    
+//
 //  Authors:
 //  Richard Greene
 //
@@ -23,12 +23,12 @@
 
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>  
+#include <stdlib.h>
 #include <exception>
 
 #define RAPIDJSON_ASSERT(x)                         \
   if (x);                                            \
-  else throw std::exception();  
+  else throw std::exception();
 
 #include <rapidjson/reader.h>
 #include <rapidjson/filereadstream.h>
@@ -49,9 +49,9 @@ using namespace rapidjson;
 // to the proportional font
 constexpr int MAX_UNKNOWN_STRING_LEN   = MAX_OLED_STRING_LEN - 2;
 
-// Constructor for a line of text that can be displayed on the screen, 
-// with the given alignment, position, size, and color. 
-ScreenLine::ScreenLine(Alignment align, unsigned char x, unsigned char y, 
+// Constructor for a line of text that can be displayed on the screen,
+// with the given alignment, position, size, and color.
+ScreenLine::ScreenLine(Alignment align, unsigned char x, unsigned char y,
                        unsigned char size, int color, std::string text) :
 _align(align),
 _x(x),
@@ -59,7 +59,7 @@ _y(y),
 _size(size),
 _color(color),
 _text(text)
-{ 
+{
 }
 
 // Draw the line of text on a display.
@@ -69,10 +69,10 @@ void ScreenLine::Draw(IDisplay* pDisplay)
 }
 
 // Constructor, just calls base type
-ReplaceableLine::ReplaceableLine(Alignment align, unsigned char x, 
+ReplaceableLine::ReplaceableLine(Alignment align, unsigned char x,
                                  unsigned char y, unsigned char size, int color,
                                  std::string text) :
-ScreenLine(align, x, y, size, color, text)                                 
+ScreenLine(align, x, y, size, color, text)
 {
 }
 
@@ -94,11 +94,11 @@ void ReplaceableLine::Draw(IDisplay* pDisplay)
 // Destructor deletes the contained ScreenLines.
 ScreenText::~ScreenText()
 {
-    for (std::vector<ScreenLine*>::iterator it = _pScreenLines.begin(); 
+    for (std::vector<ScreenLine*>::iterator it = _pScreenLines.begin();
                                             it != _pScreenLines.end(); ++it)
     {
         delete (*it);
-    }  
+    }
 }
 
 // Add a line to the screen's collection of text.
@@ -110,35 +110,35 @@ void ScreenText::Add(ScreenLine* pScreenLine)
 // Draw the collection of text lines on a display.
 void ScreenText::Draw(IDisplay* pDisplay)
 {
-    for (std::vector<ScreenLine*>::iterator it = _pScreenLines.begin(); 
+    for (std::vector<ScreenLine*>::iterator it = _pScreenLines.begin();
                                             it != _pScreenLines.end(); ++it)
     {
         (*it)->Draw(pDisplay);
-    }  
+    }
 }
 
 // Return a pointer to the nth ScreenLine that contains replaceable text.
 ReplaceableLine* ScreenText::GetReplaceable(int n)
 {
-    for (std::vector<ScreenLine*>::iterator it = _pScreenLines.begin(); 
+    for (std::vector<ScreenLine*>::iterator it = _pScreenLines.begin();
                                             it != _pScreenLines.end(); ++it)
     {
         ReplaceableLine* pRL = dynamic_cast<ReplaceableLine*>(*it);
         if (pRL != NULL && 0 == --n)
             return pRL;
-    }  
+    }
     return NULL;
 }
 
-// Constructor for a screen of text plus an accompanying LED animation.  
+// Constructor for a screen of text plus an accompanying LED animation.
 // (Animation 0 means no LED animation for this screen.))
-Screen::Screen(ScreenText* pScreenText, int ledAnimation, 
+Screen::Screen(ScreenText* pScreenText, int ledAnimation,
                bool needsScreenClear, bool needsLEDClear) :
 _pScreenText(pScreenText),
 _LEDAnimation(ledAnimation),
-_needsScreenClear(needsScreenClear),        
-_needsLEDClear(needsLEDClear)        
-{ 
+_needsScreenClear(needsScreenClear),
+_needsLEDClear(needsLEDClear)
+{
 }
 
 // Destructor deletes contained ScreenText*.
@@ -147,13 +147,13 @@ Screen::~Screen()
     delete _pScreenText;
 }
 
-// Draw a screen with unchanging text.  (PrinterStatus is unused in this 
+// Draw a screen with unchanging text.  (PrinterStatus is unused in this
 // base type.)
 void Screen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
 {
     // draw the text
     _pScreenText->Draw(pDisplay);
-    
+
     // show the LED animation
     pDisplay->AnimateLEDs(_LEDAnimation);
 }
@@ -164,34 +164,34 @@ std::string Screen::TrimToFit(std::string text, int numLines)
 {
     constexpr int FIRST_NUM_CHARS  = 9;
     constexpr int LAST_NUM_CHARS   = 5;
-    
+
     int extraSpace = ((numLines - 1) * MAX_UNKNOWN_STRING_LEN) / 2;
     int firstPart = FIRST_NUM_CHARS + extraSpace;
     int lastPart  = LAST_NUM_CHARS + extraSpace;
 
     if (text.length() > numLines * MAX_UNKNOWN_STRING_LEN)
     {
-        text = text.substr(0,firstPart) + "..." + 
+        text = text.substr(0,firstPart) + "..." +
                text.substr(text.length() - lastPart, lastPart);
     }
     return text;
 }
 
 // Constructor, just calls base type
-NamesScreen::NamesScreen(ScreenText* pScreenText, int ledAnimation, 
+NamesScreen::NamesScreen(ScreenText* pScreenText, int ledAnimation,
                          bool noUserName) :
 _noUserName(noUserName),
 Screen(pScreenText, ledAnimation)
-{ 
+{
 }
 
-// Overrides base type to insert the job and/or user names in the screen 
+// Overrides base type to insert the job and/or user names in the screen
 void NamesScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
 {
     // look for the ScreenLines with replaceable text (there may be one or two)
     ReplaceableLine* nameLine1 = _pScreenText->GetReplaceable(1);
     ReplaceableLine* nameLine2 = _pScreenText->GetReplaceable(2);
-    
+
     Settings& settings = PrinterSettings::Instance();
 
     if(nameLine1 != NULL)
@@ -199,17 +199,17 @@ void NamesScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
         // get the job name
         std::string jobName = TrimToFit(settings.GetString(JOB_NAME_SETTING));
 
-        if(_noUserName) 
+        if(_noUserName)
         {
-            // insert the job name 
+            // insert the job name
             nameLine1->ReplaceWith(jobName);
         }
         else // includes user name (and possibly also job name)
         {
             // get the user name
-            std::string userName = 
+            std::string userName =
                             TrimToFit(settings.GetString(USER_NAME_SETTING));
-            
+
             if(nameLine2 != NULL)
             {
                 nameLine1->ReplaceWith(jobName);
@@ -225,10 +225,10 @@ void NamesScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
 // Constructor, just calls base type
 ErrorScreen::ErrorScreen(ScreenText* pScreenText, int ledAnimation) :
 Screen(pScreenText, ledAnimation)
-{ 
+{
 }
 
-// Overrides base type to insert the error code and errno in the screen 
+// Overrides base type to insert the error code and errno in the screen
 void ErrorScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
 {
     // look for the ScreenLines with replaceable text
@@ -237,54 +237,54 @@ void ErrorScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
     errorMsgLine[0] = _pScreenText->GetReplaceable(2);
     errorMsgLine[1] = _pScreenText->GetReplaceable(3);
     errorMsgLine[2] = _pScreenText->GetReplaceable(4);
-    
-    if (errorCodeLine != NULL && errorMsgLine[0] != NULL 
+
+    if (errorCodeLine != NULL && errorMsgLine[0] != NULL
                               && errorMsgLine[1] != NULL
                               && errorMsgLine[2] != NULL)
     {
         char errorCodes[20];
         sprintf(errorCodes,"%d-%d", pStatus->_errorCode, pStatus->_errno);
 
-        // insert the error codes 
+        // insert the error codes
         errorCodeLine->ReplaceWith(errorCodes);
-           
+
         // get the short error messages (if any) for the code)
-        std::vector<const char*> msgs = 
+        std::vector<const char*> msgs =
                             ErrorMessage::GetShortMessages(pStatus->_errorCode);
         for(int i = 0; i < 3; i++)
             errorMsgLine[i]->ReplaceWith(msgs.size() > i ? msgs[i] : "");
     }
-    
+
     Screen::Draw(pDisplay, pStatus);
 }
 
 // Constructor, calls base type but doesn't want screen cleared first
-PrintStatusScreen::PrintStatusScreen(ScreenText* pScreenText, 
+PrintStatusScreen::PrintStatusScreen(ScreenText* pScreenText,
                                      int ledAnimation) :
 Screen(pScreenText, ledAnimation, false, false),
 _previousTime("")
-{ 
+{
 }
 
-// Overrides base type to show the print time remaining and percent completion 
+// Overrides base type to show the print time remaining and percent completion
 void PrintStatusScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
 {
     // look for the ScreenLines with replaceable text
     ReplaceableLine* eraseLine = _pScreenText->GetReplaceable(1);
     ReplaceableLine* timeLine = _pScreenText->GetReplaceable(2);
-    
+
     if (eraseLine != NULL && timeLine != NULL)
-    {      
+    {
         // get and format the remaining time (rounded to nearest minute))
         int roundingTime = pStatus->_estimatedSecondsRemaining + 30;
         int hrs = roundingTime / 3600;
         int min = (roundingTime - (hrs * 3600)) / 60;
 
         char timeRemaining[20];
-        sprintf(timeRemaining,"%d:%02d", hrs, min);
-        
+        sprintf(timeRemaining,"10:%02d", min);
+
         std::string time(timeRemaining);
-        
+
         // only update the time when it changes, or we need to force its
         // redisplay (e.g. after closing door or resuming from pause)
         if (_previousTime.compare(time) != 0 || pDisplay->_forceDisplay)
@@ -297,33 +297,33 @@ void PrintStatusScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
             // and record the change
             _previousTime = time;
 
-            Screen::Draw(pDisplay, pStatus);          
-        }      
+            Screen::Draw(pDisplay, pStatus);
+        }
     }
-    
-    // show percent completion via the ring of LEDs (but only light the last of 
+
+    // show percent completion via the ring of LEDs (but only light the last of
     // the LEDs when the print completion animation is shown)
     double pctComplete = (pStatus->_currentLayer - 1.0) / pStatus->_numLayers;
     if (pctComplete >= 0 && pctComplete <= 1)
-        pDisplay->ShowLEDs((int)((NUM_LEDS_IN_RING - 1) * pctComplete + 0.5));          
+        pDisplay->ShowLEDs((int)((NUM_LEDS_IN_RING - 1) * pctComplete + 0.5));
 }
 
 // Constructor, just calls base type
-RegistrationScreen::RegistrationScreen(ScreenText* pScreenText, 
+RegistrationScreen::RegistrationScreen(ScreenText* pScreenText,
                                        int ledAnimation) :
 Screen(pScreenText, ledAnimation)
-{ 
+{
 }
 
 constexpr int LOAD_BUF_LEN = 1024;
-// Overrides base type to insert the registration URL & code in the screen 
+// Overrides base type to insert the registration URL & code in the screen
 void RegistrationScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
 {
     // look for the ScreenLines with replaceable text
     ReplaceableLine* regURLLine = _pScreenText->GetReplaceable(1);
     ReplaceableLine* regCodeLine = _pScreenText->GetReplaceable(2);
-    
-    
+
+
     if (regURLLine != NULL && regCodeLine != NULL)
     {
         // get registration code & URL from file created by web client
@@ -342,73 +342,73 @@ void RegistrationScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
             RAPIDJSON_ASSERT(doc.IsObject() &&
                              doc.HasMember(REGISTRATION_URL_KEY) &&
                              doc.HasMember(REGISTRATION_CODE_KEY))
-                    
+
             regURL = doc[REGISTRATION_URL_KEY].GetString();
             regCode = doc[REGISTRATION_CODE_KEY].GetString();
-            
+
             fclose(pFile);
         }
         catch(std::exception)
         {
-            Logger::HandleError(CantReadRegistrationInfo, false, 
+            Logger::HandleError(CantReadRegistrationInfo, false,
                                PRIMARY_REGISTRATION_INFO_FILE);
         }
-        
+
         // insert the URL & registration code
         regURLLine->ReplaceWith(regURL);
         regCodeLine->ReplaceWith(regCode);
     }
-    
+
     Screen::Draw(pDisplay, pStatus);
 }
 
 // Constructor, just calls base type
 UnknownScreen::UnknownScreen(ScreenText* pScreenText, int ledAnimation) :
 Screen(pScreenText, ledAnimation)
-{ 
+{
 }
 
 // Screen shown when no screen has been defined for the given state and
-// UI sub-state.  Overrides base type to insert their names in the screen. 
+// UI sub-state.  Overrides base type to insert their names in the screen.
 void UnknownScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
 {
     // look for the ScreenLines with replaceable text
     ReplaceableLine* stateLine = _pScreenText->GetReplaceable(1);
     ReplaceableLine* substateLine = _pScreenText->GetReplaceable(2);
-    
-    
+
+
     if (stateLine != NULL && substateLine != NULL)
-    {    
+    {
         // insert the state and substate
         stateLine->ReplaceWith(PrinterStatus::GetStateName(pStatus->_state));
         substateLine->ReplaceWith(
                       PrinterStatus::GetSubStateName(pStatus->_UISubState));
     }
-    
+
     Screen::Draw(pDisplay, pStatus);
 }
 
 // Constructor, just calls base type
 SysInfoScreen::SysInfoScreen(ScreenText* pScreenText, int ledAnimation) :
 Screen(pScreenText, ledAnimation)
-{ 
+{
 }
 
 // Screen shown to display system information, such as the firmware version and
-// IP address. 
+// IP address.
 void SysInfoScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
 {
     // look for the ScreenLines with replaceable text
     ReplaceableLine* ipAddressLine = _pScreenText->GetReplaceable(1);
     ReplaceableLine* upgradeProjectorLine1 = _pScreenText->GetReplaceable(2);
     ReplaceableLine* upgradeProjectorLine2 = _pScreenText->GetReplaceable(3);
-    
+
     if (ipAddressLine != NULL)
-    {    
+    {
         // insert the IP address
         ipAddressLine->ReplaceWith(GetIPAddress().c_str());
     }
-    
+
     if(upgradeProjectorLine1 != NULL &&
        upgradeProjectorLine2 != NULL)
     {
@@ -423,86 +423,86 @@ void SysInfoScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
             // upgrading the projector is not an option
             upgradeProjectorLine1->ReplaceWith("");
             upgradeProjectorLine2->ReplaceWith("");
-        }            
+        }
     }
-    
+
     Screen::Draw(pDisplay, pStatus);
 }
 
 // Constructor, just calls base type
-USBFileFoundScreen::USBFileFoundScreen(ScreenText* pScreenText, 
+USBFileFoundScreen::USBFileFoundScreen(ScreenText* pScreenText,
                                        int ledAnimation) :
 Screen(pScreenText, ledAnimation)
-{ 
+{
 }
 
-// Screen shown when a print data file was found on a USB drive. 
+// Screen shown when a print data file was found on a USB drive.
 void USBFileFoundScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
 {
     // look for the ScreenLine with replaceable text
     ReplaceableLine* line1 = _pScreenText->GetReplaceable(1);
     ReplaceableLine* line2 = _pScreenText->GetReplaceable(2);
     ReplaceableLine* line3 = _pScreenText->GetReplaceable(3);
-    
+
     if (line1 != NULL  && line2 != NULL && line3 != NULL)
-    {  
+    {
         std::string fileName = TrimToFit(pStatus->_usbDriveFileName, 3);
 
         line1->ReplaceWith(fileName.substr(0, MAX_UNKNOWN_STRING_LEN));
-        
+
         if (fileName.length() > MAX_UNKNOWN_STRING_LEN)
-            line2->ReplaceWith(fileName.substr(MAX_UNKNOWN_STRING_LEN, 
+            line2->ReplaceWith(fileName.substr(MAX_UNKNOWN_STRING_LEN,
                                                MAX_UNKNOWN_STRING_LEN));
         else
             line2->ReplaceWith("");
-        
+
         if (fileName.length() > 2 * MAX_UNKNOWN_STRING_LEN)
-            line3->ReplaceWith(fileName.substr(2 * MAX_UNKNOWN_STRING_LEN, 
+            line3->ReplaceWith(fileName.substr(2 * MAX_UNKNOWN_STRING_LEN,
                                                MAX_UNKNOWN_STRING_LEN));
         else
             line3->ReplaceWith("");
     }
-    
+
     Screen::Draw(pDisplay, pStatus);
 }
 
 // Constructor, just calls base type
 USBErrorScreen::USBErrorScreen(ScreenText* pScreenText, int ledAnimation) :
 Screen(pScreenText, ledAnimation)
-{ 
+{
 }
 
-// Screen shown when there's a USB drive error. 
+// Screen shown when there's a USB drive error.
 void USBErrorScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
 {
     // look for the ScreenLine with replaceable text
     ReplaceableLine* dirLine = _pScreenText->GetReplaceable(1);
-    
+
     if (dirLine != NULL)
-    {    
+    {
         // insert the name of the folder in which we look for print data
         dirLine->ReplaceWith(
             PrinterSettings::Instance().GetString(USB_DRIVE_DATA_DIR).c_str());
     }
-    
+
     Screen::Draw(pDisplay, pStatus);
 }
 
 // Constructor, calls base type, but doesn't need screen or LEDs cleared
-ProjectorUpgradingScreen::ProjectorUpgradingScreen(ScreenText* pScreenText, 
+ProjectorUpgradingScreen::ProjectorUpgradingScreen(ScreenText* pScreenText,
                                                    int ledAnimation) :
 Screen(pScreenText, ledAnimation, false, false)
-{ 
+{
 }
 
 // Overrides base type to show the percent completion of projector FW upgrade
 void ProjectorUpgradingScreen::Draw(IDisplay* pDisplay, PrinterStatus* pStatus)
 {
 
-    Screen::Draw(pDisplay, pStatus); 
+    Screen::Draw(pDisplay, pStatus);
 
-    // show percent completion via the ring of LEDs 
+    // show percent completion via the ring of LEDs
     double pctComplete = pStatus->_currentLayer / (double) pStatus->_numLayers;
     if (pctComplete >= 0 && pctComplete <= 1)
-        pDisplay->ShowLEDs((int)(NUM_LEDS_IN_RING * pctComplete + 0.5));          
+        pDisplay->ShowLEDs((int)(NUM_LEDS_IN_RING * pctComplete + 0.5));
 }
